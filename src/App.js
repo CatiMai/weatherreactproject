14186375340d2b3a4f31ab8625a8988b0f012114
CatiMai <77164603+CatiMai@users.noreply.github.com>
2021-03-11 12:18:46 +0100

    added bootstrap to project + added footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <p>This will become a weather app</p>
+    <div className="App container">
+      <footer>
+        <a
+          href="https://github.com/CatiMai/weatherreactproject"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open Source Code
+        </a>{" "}
+        by Caterina Maidhof
+      </footer>
     </div>
   );
 }
