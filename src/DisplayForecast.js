a86155268cbe59cffba39dc61664c3b1813166a6
CatiMai <77164603+CatiMai@users.noreply.github.com>
2021-03-17 16:59:33 +0100

    update forecast

--- src/DisplayForecast.js
+++ src/DisplayForecast.js
@@ -2,19 +2,28 @@ import React from "react";
 import DisplayIcon from "./DisplayIcon";
 
 export default function DisplayForecast(props) {
+  let iconPath = props.weather[0].icon;
   function hours() {
     let date = new Date(props.dt * 1000);
     let hours = date.getHours();
     return `${hours}:00`;
   }
+  function temp() {
+    let temperature = Math.round(props.main.temp);
+    return temperature;
+  }
   return (
     <div>
       <div className="col-2">
         {hours()}
-        <div className="ForecastIcon"></div>
-        {Math.round(props.main.temp)}°C
+        <div className="ForecastIcon">
+          <>
+            <DisplayIcon code={iconPath} />
+          </>
+        </div>
+        {temp()}°C
       </div>
     </div>
   );
 }
-// <DisplayIcon code={props.weather[0].icon} />
+//
